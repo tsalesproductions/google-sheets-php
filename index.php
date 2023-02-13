8417d7e3ff6abc77d4674014ec5e129e8216a65a
Thiago Sales <thiago_salests@hotmail.com>
2023-02-13 11:31:43 -0300

    base

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+# RESOLVER ERRO SSL
+    # https://stackoverflow.com/questions/35638497/curl-error-60-ssl-certificate-prblm-unable-to-get-local-issuer-certificate
+
+#GOOGLE SHEET PHP
+    # https://developers.google.com/sheets/api/guides/values
+
+#DELETAR ITEM
+    # https://stackoverflow.com/questions/41000763/how-can-i-delete-rows-from-google-sheet-with-php
+
 /**
  * https://console.cloud.google.com/iam-admin/serviceaccounts
  * Copyright 2022 Google Inc.
@@ -17,10 +26,33 @@
  */
 
 // [START sheets_append_values]
-header('Content-Type: application/json; charset=utf-8');
+// header('Content-Type: application/json; charset=utf-8');
 use Google\Client;
+use Google\Service\Drive;
 use Google\Service\Sheets;
 
+require 'vendor/autoload.php';
+
+# ID DA PLANILHA
+define("sheet_id", '1ruzFijAtYDaY3HWO45lDA7kYUBkph_g9IIkqL9-ntDc');
+
+# LOCAL ONDE ESTÁ SALVO AS CREDÊNCIAIS
+define('auth_config_file', __DIR__.'/auth.json');
+
+# STATUS DA APLICAÇÃO: offline=local;online=produção;
+define("application_access_type", "offline");
+
+# NOME DA APLICAÇÃO
+define("application_name", 'Google Sheets API PHP Quickstart');
+
+define("vtex_host", "https://tfcvvp.vtexcommercestable.com.br");
+define("vtex_app_id", "vtexappkey-tfcvvp-IVYQAD");
+define("vtex_app_token", "PRLFPBJOCWWAWGRZHGVSFNYPHNTBQBLFGXQCXOCVFXJNWYAPNHYYHMIBTWXBIIYBLRTZQOBSDZNIEKSJNENICMXUOOJBQANWYHCFEBZZFFVHYAWIDNODVQYYTZLBEACC");
+
+# QUANTIDADE DE COLUNAS QUE A TABELA IRÁ BUSCAR: 
+# A2:D999 = QUERO QUE ME TRAGA AS LINHAS DE A2(PARA IGNORAR OS TÍTULOS) ATÉ O D(MINHA ÚLTIMA COLUNA). 
+# ALÉM DISSO QUERO QUE ELE ME TRAGA OS PRIMEIROS 999 RESULTADOS DESSE DOCUMENTO
+define("sheet_range_find", 'A6:F999');
 
 function appendValues($spreadsheetId, $range, $valueInputOption, $data)
 {
@@ -28,8 +60,10 @@ function appendValues($spreadsheetId, $range, $valueInputOption, $data)
        TODO(developer) - See https://developers.google.com/identity for
         guides on implementing OAuth2 for your application. */
     $client = new Google\Client();
-    $client->setAuthConfig(__DIR__.'/auth.json');
-    $client->addScope('https://www.googleapis.com/auth/spreadsheets');
+    $client->setApplicationName(application_name);
+    $client->setScopes('https://www.googleapis.com/auth/spreadsheets');
+    $client->setAuthConfig(auth_config_file);
+    $client->setAccessType(application_access_type);
     $service = new Google\Service\Sheets($client);
     try {
         $values = [$data]; //add the values to be appended
@@ -47,6 +81,7 @@ function appendValues($spreadsheetId, $range, $valueInputOption, $data)
             'data' => $result
         ]);
     } catch (Exception $e) {
+        var_dump($e->getMessage());
         return json_encode([
             'status'=> false,
             'data' => $e->getMessage()
@@ -55,19 +90,184 @@ function appendValues($spreadsheetId, $range, $valueInputOption, $data)
     // [END sheets_append_values]
 }
 
-require 'vendor/autoload.php';
-// appendValues('1-JrsMcc7DpPnoorb0q2PiCNwtIZ_hduBrbQuYjq0ko8', 'A1:B2', "RAW", ["Thiago", "foi", "acampar","la"]);
-
-if($_SERVER['REQUEST_METHOD'] === "POST"){
-    if(!isset($_POST['planilha_id']) && !isset($_POST['data'])){
-        echo json_encode([
-            'status'=> false,
-            'msg' => 'campos planilha_id e data são obrigatórios e precisam ter valores'
-        ]);
-        return;
-    }else{
-        echo appendValues($_POST['planilha_id'], 'A1:B2', "RAW", $_POST['data']);
+function batchGetValues($spreadsheetId)
+{
+    /* Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity for
+    guides on implementing OAuth2 for your application. */       
+    $client = new Google\Client();
+    $client->setApplicationName(application_name);
+    $client->setScopes('https://www.googleapis.com/auth/spreadsheets');
+    $client->setAuthConfig(auth_config_file);
+    $client->setAccessType(application_access_type);
+    $client->addScope(Google\Service\Drive::DRIVE);
+    $service = new Google_Service_Sheets($client);
+    try{
+        $ranges = sheet_range_find;
+        $params = array(
+            'ranges' => $ranges,
+        );
+        //execute the request
+        $result = $service->spreadsheets_values->batchGet($spreadsheetId, $params);
+        // printf("%d ranges retrieved.", count($result->getValueRanges()));
+        return $result;
+    }
+    catch(Exception $e) {
+        // TODO(developer) - handle error appropriately
+        echo 'Message: ' .$e->getMessage();
     }
 }
 
+# ASSOCIA E ORGANIZA AS CATEGORIAS E SUB CATEGORIAS
+function getListCategoryes(){
+    $values = batchGetValues(sheet_id)->valueRanges[0]->values;
 
+    $categorieswithChild = [];
+
+    $last = '';
+
+    foreach ($values as $key => $value) {
+        //$has = array_search($needle, $value);
+        if($value){
+            $found_key = array_search($value[2], array_column($categorieswithChild, 'name'));
+            if($found_key == false && $value[2] !== ''){
+                array_push($categorieswithChild, [
+                    'name' => $value[2],
+                    'title' => $value[3],
+                    'meta_title' => $value[4],
+                    'meta_description' => $value[5],
+                    'childs' => []
+                ]);
+            }
+        }
+    }
+
+    // var_dump($categorieswithChild);
+
+    foreach ($values as $key => $value) {
+        if($value){
+            if($value[2] !== ''){
+                $last = $value[2];
+            }else{
+                // echo $last.'->'.$value[3].'<br>  ';
+                $seachCategory = array_search($last, array_column($categorieswithChild, 'name'));
+                if($seachCategory !== false){
+                    $childrens = $categorieswithChild[$seachCategory]['childs'];
+                    $searchChildren = array_search($value[3], array_column($childrens, 'name'));
+                    if($searchChildren == false &&  isset($value[5])){
+                        array_push($categorieswithChild[$seachCategory]['childs'], [
+                            'name' => $value[3],
+                            'title' => $value[3],
+                            'meta_title' => $value[4],
+                            'meta_description' => $value[5],
+                        ]);
+                    }
+                }
+            } 
+        }
+    }
+
+    return $categorieswithChild;
+    
+}
+
+# FAZ A REQUISIÇÃO GET
+# https://packagist.org/packages/guzzlehttp/guzzle
+function getdata($url,$headers=[]) 
+{ 
+    $client = new \GuzzleHttp\Client();
+    $response = $client->request('GET', $url, [
+        'headers' => [
+            "Content-Type: application/json",
+            "Accept: application/json",
+            "X-VTEX-API-AppKey: ".vtex_app_id,
+            "X-VTEX-API-AppToken: ".vtex_app_token
+        ]        
+    ]);
+
+    return json_decode($response->getBody(), true);
+}
+
+# CRIA A CATEGORIA PRIMÁRIA; RETORNA O ID APÓS CRIADO;
+# TAMBÉM É REAPROVEITADA PARA CRIAR A SUBCATEGORIA FILHO
+function createFirstStageCategory($data, $fatherCategoryId = null){
+    $body = '{
+        "Name":"'.strtoupper($data['name']).'",
+        "FatherCategoryId":"'.$fatherCategoryId.'",
+        "Title":"'.$data['title'].'",
+        "Description":"'.$data['meta_title'].'",
+        "Keywords":"'.substr(implode(",", explode(" ", $data['title'])),0, -1).'",
+        "IsActive":true,
+        "LomadeeCampaignCode":null,
+        "AdWordsRemarketingCode":null,
+        "ShowInStoreFront":true,
+        "ShowBrandFilter":false,
+        "ActiveStoreFrontLink":true,
+        "GlobalCategoryId":166,
+        "StockKeepingUnitSelectionMode":"SPECIFICATION",
+        "Score":null
+    }';
+
+    $client = new \GuzzleHttp\Client();
+    $response = $client->request('POST', vtex_host."/api/catalog/pvt/category", [
+        'body'  => $body,
+        'headers' => [
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
+            "X-VTEX-API-AppKey" => vtex_app_id,
+            "X-VTEX-API-AppToken" => vtex_app_token
+        ],
+    ]);
+
+    $res = json_decode($response->getBody(), true);
+
+    if(isset($res['Id'])){
+        echo (!$fatherCategoryId ? "- Categoria" : "--Subcategoria")." criada com sucesso!<br>";
+        return $res['Id'];
+    }else{
+        return false;
+    }
+}
+
+function createSecondStageCategory($value, $vtexCList, $seachCategory){
+    $fatherCategoryId = $vtexCList[$seachCategory]['id'];
+    $childsVtex = $vtexCList[$seachCategory]['children'];
+    $childsPlan = $value['childs'];
+
+    foreach ($childsPlan as $key => $chd) {
+        $searchChild = array_search(strtoupper($chd['name']), array_column($childsVtex, 'name'));
+        if($searchChild === false){ # CASO NÃO TENHA A SUBCATEGORIA, ELE CRIA
+            createFirstStageCategory($chd, $fatherCategoryId);
+        }
+    }
+}
+
+# FAZ A CONFERÊNCIA NA API DA VTEX E TAMBÉM NA PLANILHA
+# VERIFICA SE EXISTE OU NÃO A CATEGORIA, SE NÃO EXISTIR, ELE CRIA O PRIMEIRO ESTÁGIO DA CATEGORIA E DEPOIS CRIA OS PRÓXIMOS
+# CASO EXISTA, VERIFICA O SEGUNDO ESTÁGIO E DEPOIS CRIA
+# OBS: A API DE CATEGORIAS TEM CACHE DE 5~10M, NÃO RODE NOVAMENTE ANTES DESSE TEMPO OU IRÁ DUPLICAR
+function createOrUpdateCategory(){
+    $vtexCList = getdata(vtex_host."/api/catalog_system/pub/category/tree/1"); # BUSCA A API DA VTEX
+    
+    // var_dump($vtexCList);
+
+    foreach (getListCategoryes() as $key => $value) {
+        if($key >= 1) break;
+        $seachCategory = array_search(strtoupper($value['name']), array_column($vtexCList, 'name'));
+
+        if($seachCategory === false){ # caso não tenha a categoria primária
+            $fId = createFirstStageCategory($value); # CRIA A CATEGORIA CRIADA
+
+            if($fId){ # SE CRIAR A PRIMÁRIA CORRETAMENTE, AGORA CRIA AS SUBS
+                foreach ($value['childs'] as $key => $chd) {
+                    createFirstStageCategory($chd, $fId);
+                }
+            }
+        }else{ #caso tenha, verifica e cria o segundo estágio das categorias
+            createSecondStageCategory($value, $vtexCList, $seachCategory);
+        }
+        
+    }
+}
+
+createOrUpdateCategory();
--- readme.md
+++ readme.md
@@ -1,44 +1,33 @@
-### google-sheets-php
-Projeto criado para trabalhar com inserção de dados em planilhas do google com PHP;
+### [google-sheets-php](https://github.com/tsalesproductions/google-sheets-php)
+Projeto criado para trabalhar com inserção de dados em planilhas do google com PHP; 
+E ADAPTADO PARA CRIAÇÃO DE CATEGORIAS NA VTEX;
 
 # Requisitos
 - PHP >= 7.3
 - [Composer](https://getcomposer.org/)
 
+# OBS
+1. A planilha deve estar no modo público
+2. A api da VTEX tem cache de 5~10 minutos para retornar as novas categorias criadas, não rode o script novamente antes desse tempo, caso contrário, irá duplicar as categorias.
+3. Seguir o modelo de planilha: `Breve`
+4. O foi comentado informando como foi criado, e a lógica por cima.
+
 # Como usar
-1. Acesse o [Console do Google - API](https://console.cloud.google.com/apis/dashboard), ative a API **Google Sheets API**
-1. Acesse o [Console do Google - Credênciais](https://console.cloud.google.com/apis/credentials), crie uma credencial e copie o código
-1. Acesse o [Console do Google - IAM ADMIN](https://console.cloud.google.com/iam-admin/serviceaccounts), crie um novo projeto, acesse-o, navegue até guia **chaves** e adicione uma chave json, baixe-a e separe.
-1. Clone o repositório, insira a chave dentro do repositório com o nome: **auth.json**.
-1. Execute o comando `composer install` para instalar as dependências
-1. Crie e configure a sua planilha em modo público e modo editor para quem possuir o link;
-
-Agora basta realizar os testes...
-Usei uma requisição AJAX para estar efetuando a integração via API
-
-`/req.html`
-```JS
-window.addEventListener("load", () => {
-            let result = $.post('http://localhost/google-planilhas/', {
-                planilha_id: '1-JrsMcc7DpPnoorb0q2PdsaiCNwtIZ_hduBrbQuYjq0ko8',
-                data: [
-                    "Thiago Sales",
-                    "thsales1997",
-                    "27/04/1997",
-                    "Rio de Janeiro"
-                ]
-            }).done((result) => {
-                console.log(result)
-            })
-        })
-```
-Obs: necessário passar o id da planilha e o array contendo os valores das colunas.
-O id da planilha você obtem na url, após o /d/
+1. Abra o `index.php`
+1. Procure por `sheet_id` e troque pelo id da sua planilha, veja o exemplo abaixo, pegando a id via URL
 ```txt
 https://docs.google.com/spreadsheets/d/
-1-JrsMcc7DpPnoorb0q2PdsaiCNwtIZ_hduBrbQuYjq0ko8 // < Um exemplo
-/
+1-JrsMcc7DpPnoorb0q2PdsaiCNwtIZ_hduBrbQuYjq0ko8 // < ESTE É O ID
 ```
+1. Procure por `vtex_host` `vtex_app_id` `vtex_app_token` e troque as credênciais dos clientes
+1. A linha `sheet_range_find` É responsável por pegar as colunas. Entenda abaixo:
+```text
+# QUANTIDADE DE COLUNAS QUE A TABELA IRÁ BUSCAR: 
+# A2:D999 = QUERO QUE ME TRAGA AS LINHAS DE A2(PARA IGNORAR OS TÍTULOS) ATÉ O D(MINHA ÚLTIMA COLUNA). 
+# ALÉM DISSO QUERO QUE ELE ME TRAGA OS PRIMEIROS 999 RESULTADOS DESSE DOCUMENTO
+define("sheet_range_find", 'A6:F999');
+```
+1. Após trocar os dados, rode uma vez o index.php e confira se irá funcionar corretamente.
 
 ### Licença
 MIT
